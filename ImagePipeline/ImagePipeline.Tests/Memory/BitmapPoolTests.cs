16b21929d380f0bb42a22dd37d9c4fd9f0cf752f
Phong Cao <phong.caothai@live.com>
2016-09-07 00:08:13 -0400

    Updated unit tests.

--- ImagePipeline/ImagePipeline.Tests/Memory/BitmapPoolTests.cs
+++ ImagePipeline/ImagePipeline.Tests/Memory/BitmapPoolTests.cs
@@ -29,21 +29,23 @@ namespace ImagePipeline.Tests.Memory
         [TestMethod]
         public void TestFree()
         {
-            SoftwareBitmap bitmap = _pool.Alloc(12);
-            _pool.Free(bitmap);
-            bool disposed = false;
-
-            // Check if object has been disposed
-            try
+            using (SoftwareBitmap bitmap = _pool.Alloc(12))
             {
-                disposed = (bitmap.PixelWidth == 0);
-            }
-            catch (ObjectDisposedException e)
-            {
-                disposed = true;
-            }
+                _pool.Free(bitmap);
+                bool disposed = false;
 
-            Assert.IsTrue(disposed);
+                // Check if object has been disposed
+                try
+                {
+                    disposed = (bitmap.PixelWidth == 0);
+                }
+                catch (ObjectDisposedException e)
+                {
+                    disposed = true;
+                }
+
+                Assert.IsTrue(disposed);
+            }
         }
 
         // Tests out the GetBucketedSize method
@@ -58,14 +60,16 @@ namespace ImagePipeline.Tests.Memory
         [TestMethod]
         public void TestGetBucketedSizeForValue()
         {
-            SoftwareBitmap bitmap1 = _pool.Alloc(12);
-            SoftwareBitmap bitmap2 = _pool.Alloc(56);
-            SoftwareBitmap bitmap3 = MockBitmapFactory.Create(7, 8, BitmapPixelFormat.Rgba8);
-            SoftwareBitmap bitmap4 = MockBitmapFactory.Create(7, 8, BitmapPixelFormat.Rgba16);
-            Assert.AreEqual(12, _pool.GetBucketedSizeForValue(bitmap1));
-            Assert.AreEqual(56, _pool.GetBucketedSizeForValue(bitmap2));
-            Assert.AreEqual(224, _pool.GetBucketedSizeForValue(bitmap3));
-            Assert.AreEqual(448, _pool.GetBucketedSizeForValue(bitmap4));
+            using (SoftwareBitmap bitmap1 = _pool.Alloc(12),
+                                  bitmap2 = _pool.Alloc(56),
+                                  bitmap3 = MockBitmapFactory.Create(7, 8, BitmapPixelFormat.Rgba8),
+                                  bitmap4 = MockBitmapFactory.Create(7, 8, BitmapPixelFormat.Rgba16))
+            {
+                Assert.AreEqual(12, _pool.GetBucketedSizeForValue(bitmap1));
+                Assert.AreEqual(56, _pool.GetBucketedSizeForValue(bitmap2));
+                Assert.AreEqual(224, _pool.GetBucketedSizeForValue(bitmap3));
+                Assert.AreEqual(448, _pool.GetBucketedSizeForValue(bitmap4));
+            }
         }
 
         [TestMethod]
@@ -79,17 +83,19 @@ namespace ImagePipeline.Tests.Memory
         [TestMethod]
         public void TestIsReusable()
         {
-            SoftwareBitmap b1 = _pool.Alloc(12);
-            Assert.IsTrue(_pool.IsReusable(b1));
-            SoftwareBitmap b2 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Bgra8);
-            Assert.IsTrue(_pool.IsReusable(b2));
-            SoftwareBitmap b3 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Gray16);
-            Assert.IsTrue(_pool.IsReusable(b3));
-            SoftwareBitmap b4 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Bgra8);
-            b4.Dispose();
-            Assert.IsFalse(_pool.IsReusable(b4));
-            SoftwareBitmap b5 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Bgra8).GetReadOnlyView();
-            Assert.IsFalse(_pool.IsReusable(b5));
+            using (SoftwareBitmap b1 = _pool.Alloc(12),
+                                  b2 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Bgra8),
+                                  b3 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Gray16),
+                                  b4 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Bgra8),
+                                  b5 = MockBitmapFactory.Create(3, 4, BitmapPixelFormat.Bgra8).GetReadOnlyView())
+            {
+                Assert.IsTrue(_pool.IsReusable(b1));
+                Assert.IsTrue(_pool.IsReusable(b2));
+                Assert.IsTrue(_pool.IsReusable(b3));
+                b4.Dispose();
+                Assert.IsFalse(_pool.IsReusable(b4));
+                Assert.IsFalse(_pool.IsReusable(b5));
+            }
         }
     }
 }
