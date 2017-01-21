04e6fe74376aba210ded1a80a853e889e74dfaf7
Phong Cao <phong.caothai@live.com>
2017-01-21 15:15:01 -0500

    Added NullProducer and unit tests.

---
+++ ImagePipeline/ImagePipeline.Tests/Producers/NullProducerTests.cs
@@ -0,0 +1,77 @@
+﻿using ImagePipeline.Common;
+using ImagePipeline.Producers;
+using ImagePipeline.Request;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace ImagePipeline.Tests.Producers
+{
+    /// <summary>
+    /// Checks basic properties of NullProducer, that is that it always returns null.
+    /// </summary>
+    [TestClass]
+    public class NullProducerTests
+    {
+        private readonly ImageRequest IMAGE_REQUEST = ImageRequest.FromUri("http://microsoft.com");
+        private const string REQUEST_ID = "RequestId";
+
+        private IConsumer<object> _consumer;
+        private IProducerContext _producerContext;
+        private IProducerListener _producerListener;
+        private NullProducer<object> _nullProducer;
+        private int _consumerOnNewResultCount;
+        private object _consumerInternalResult;
+        private bool _consumerInternalIsLast;
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Initializes the mock data
+            _producerListener = new ProducerListenerImpl(
+                (_, __) => { },
+                (_, __, ___) => { },
+                (_, __, ___) => { },
+                (_, __, ___, ____) => { },
+                (_, __, ___) => { },
+                (_) =>
+                {
+                    return false;
+                });
+            _consumer = new BaseConsumerImpl<object>(
+                (result, isLast) =>
+                {
+                    ++_consumerOnNewResultCount;
+                    _consumerInternalResult = result;
+                    _consumerInternalIsLast = isLast;
+                },
+                (_) => {},
+                () => {},
+                (_) => {});
+            _producerContext = new SettableProducerContext(
+                IMAGE_REQUEST,
+                REQUEST_ID,
+                _producerListener,
+                new object(),
+                RequestLevel.FULL_FETCH,
+                false,
+                true,
+                Priority.MEDIUM);
+
+            _nullProducer = new NullProducer<object>();
+        }
+
+        /// <summary>
+        /// Tests the result from OnNewResult
+        /// </summary>
+        [TestMethod]
+        public void TestNullProducerReturnsNull()
+        {
+            _nullProducer.ProduceResults(_consumer, _producerContext);
+            Assert.IsTrue(_consumerOnNewResultCount == 1);
+            Assert.IsNull(_consumerInternalResult);
+            Assert.IsTrue(_consumerInternalIsLast);
+        }
+    }
+}
---
+++ ImagePipeline/ImagePipeline/Producers/NullProducer.cs
@@ -0,0 +1,22 @@
+﻿namespace ImagePipeline.Producers
+{
+    /// <summary>
+    /// Producer that never produces anything, but just returns null.
+    ///
+    /// <para />This producer can be used to terminate a sequence, e.g. for a bitmap cache 
+    /// get only sequence, just use BitmapMemoryCacheGetProducer followed by NullProducer.
+    /// </summary>
+    public class NullProducer<T> : IProducer<T>
+    {
+        /// <summary>
+        /// Start producing results for given context. Provided consumer is notified whenever 
+        /// progress is made (new value is ready or error occurs).
+        /// <param name="consumer"></param>
+        /// <param name="context"></param>
+        /// </summary>
+        public void ProduceResults(IConsumer<T> consumer, IProducerContext context)
+        {
+            consumer.OnNewResult(default(T), true);
+        }
+    }
+}
