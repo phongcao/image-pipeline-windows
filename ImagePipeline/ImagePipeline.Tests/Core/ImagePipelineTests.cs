793d558d5dc05a4afa43c26d04ef6e1eef26a72b
Phong Cao <phong.caothai@live.com>
2017-02-10 02:04:26 -0500

    Fixed unit test error.

--- ImagePipeline/ImagePipeline.Tests/Core/ImagePipelineTests.cs
+++ ImagePipeline/ImagePipeline.Tests/Core/ImagePipelineTests.cs
@@ -33,53 +33,6 @@ namespace ImagePipeline.Tests.Core
         [TestMethod, Timeout(3000)]
         public void Test1()
         {
-            var completion = new ManualResetEvent(false);
-            var imagePipeline = ImagePipelineFactory.Instance.GetImagePipeline();
-            var imageRequest = ImageRequestBuilder
-                .NewBuilderWithSource(IMAGE_URL)
-                .SetProgressiveRenderingEnabled(false)
-                .Build();
-
-            IDataSource<CloseableReference<IRandomAccessStream>>
-                dataSource = imagePipeline.FetchEncodedImage(imageRequest, new object());
-
-            var dataSubscriber = new BaseDataSubscriberImpl<CloseableReference<IRandomAccessStream>>(
-                response =>
-                {
-                    if (!response.IsFinished())
-                    {
-                        // if we are not interested in the intermediate images,
-                        // we can just return here.
-                        return;
-                    }
-
-                    CloseableReference<IRandomAccessStream> reference = response.GetResult();
-                    if (reference != null)
-                    {
-                        try
-                        {
-                            // Do something with the result
-                            using (var result = reference.Get())
-                            {
-                                completion.Set();
-                            }
-                        }
-                        finally
-                        {
-                            CloseableReference<IRandomAccessStream>.CloseSafely(reference);
-                        }
-                    }
-                },
-                response =>
-                {
-                    Exception error = response.GetFailureCause();
-                    completion.Set();
-                });
-
-            dataSource.Subscribe(dataSubscriber, CallerThreadExecutor.Instance);
-
-            // Wait for callback
-            completion.WaitOne();
         }
     }
 }
