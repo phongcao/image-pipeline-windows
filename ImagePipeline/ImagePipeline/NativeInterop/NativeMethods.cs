71505bcd0f69b4ed053e943c33d236df8bb9e080
Phong Cao <phong.caothai@live.com>
2017-02-28 16:47:16 -0500

    Replaced WinRT component by dll in the NativeMemoryChunk module.

---
+++ ImagePipeline/ImagePipeline/NativeInterop/NativeMethods.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ImagePipeline.NativeInterop
+{
+    internal static class NativeMethods
+    {
+        private const string DllName = "ImagePipelineNative";
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NativeFree(long lpointer);
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern long NativeAllocate(int size);
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NativeCopyToByteArray(long lpointer, byte[] byteArray, int offset, int count);
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NativeCopyFromByteArray(long lpointer, byte[] byteArray, int offset, int count);
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NativeMemcpy(long dst, long src, int count);
+
+        [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern byte NativeReadByte(long lpointer);
+    }
+}
--- ImagePipeline/ImagePipelineNative/MemChunk/NativeMemoryChunk.h
+++ ImagePipeline/ImagePipelineNative/MemChunk/NativeMemoryChunk.h
@@ -1,23 +1,26 @@
-#pragma once
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
-using namespace Platform;
+#include "Common.h"
 
+YG_EXTERN_C_BEGIN
 
-namespace ImagePipelineNative
-{
-	public ref class NativeMemoryChunk sealed
-	{
-		public:	
-			static int64 NativeAllocate(int size);
+WIN_EXPORT int64_t NativeAllocate(int size);
 
-			static void NativeFree(int64 lpointer);
+WIN_EXPORT void NativeFree(int64_t lpointer);
 
-			static void NativeCopyToByteArray(int64 lpointer, const Array<uint8>^ byteArray, int offset, int count);
+WIN_EXPORT void NativeCopyToByteArray(int64_t lpointer, uint8_t* byteArray, int offset, int count);
 
-			static void NativeCopyFromByteArray(int64 lpointer, const Array<uint8>^ byteArray, int offset, int count);
+WIN_EXPORT void NativeCopyFromByteArray(int64_t lpointer, uint8_t* byteArray, int offset, int count);
 
-			static void NativeMemcpy(int64 dst, int64 src, int count);
+WIN_EXPORT void NativeMemcpy(int64_t dst, int64_t src, int count);
 
-			static uint8 NativeReadByte(int64 lpointer);
-	};
-}
+WIN_EXPORT uint8_t NativeReadByte(int64_t lpointer);
+
+YG_EXTERN_C_END
